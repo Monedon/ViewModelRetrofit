029e5e85ebbafc9cf4ad04bbbc62d1b9f2d6be2e
Patricio García A <pgarcia@serlitoral.cl>
2020-11-29 11:11:33 -0300

    Se agregar cardview

--- app/src/main/java/cl/desafiolatam/viewmodelretrofit/adapter/HeroAdapter.java
+++ app/src/main/java/cl/desafiolatam/viewmodelretrofit/adapter/HeroAdapter.java
@@ -11,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 import java.util.List;
 
